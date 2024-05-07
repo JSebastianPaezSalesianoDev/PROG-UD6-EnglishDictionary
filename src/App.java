a5d9100bc351ec276f2e17dd6827935183e8908a
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-05-07 13:58:50 +0100

    feat(Menu) se ha añadido el metodo estatico String menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import net.duolingoinc.utility.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(Menu.Showmenu());
     }
 }
---
+++ src/net/duolingoinc/utility/Menu.java
@@ -0,0 +1,13 @@
+package net.duolingoinc.utility;
+
+public class Menu {
+    
+    public static String Showmenu(){
+
+        String menu = "******************************************************\n";
+        menu += "1. Añadir palabra\n2. eliminar palabra\n3. Existe palabra\n";
+        menu += "4 .Mostrar iniciales disponibles\n5. Ver palabras por inicial\n";
+        menu += "******************************************************\n";
+        return menu;
+    }
+}
