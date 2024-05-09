77d710bfc75cda8d61654baf4cdab2c98a236834
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-05-09 22:12:39 +0100

    reFact(dictionary) se cambiaron los metodos a estaticos

---
+++ src/net/duolingoinc/storage/Dictionary.java
@@ -0,0 +1,23 @@
+package net.duolingoinc.storage;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Dictionary {
+    
+      static private Map<String, Set<String>> mapa = new HashMap<>();
+
+      public static Map<String, Set<String>> getMapa() {
+            return Dictionary.mapa;
+      }
+
+      public static void setMapa(Map<String, Set<String>> mapa) {
+            Dictionary.mapa = mapa;
+      }
+
+      @Override
+      public String toString() {
+            return "Dictionary [mapa=" + Dictionary.mapa + "]";
+      }
+}
